803c344e72eabf40da2e9d6dd8182d3029ee7f01
barxnl-max <hibarxnl@gmail.com>
2026-01-06 21:59:35 +0800

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Knightbot MD is running ✅');
+  res.send('Cata is running ✅');
 });
 
 const PORT = process.env.PORT || 3000;
